b7d073c94f0b53d7499aaf97af6626308d7945e2
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2021-12-29 16:02:47 +0600

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var axios = require("axios").default;
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/home/:id/:name', function(req, res, next) {
@@ -7,5 +9,84 @@ router.get('/home/:id/:name', function(req, res, next) {
   res.send(req.params);
   
 });
+
+/* GET weather data from api */
+router.get('/weather/:country/:city', function(req, res, next) {
+  // Extracting URL parameters
+  var country = req.params.country;
+  var city = req.params.city;
+
+  axios.get('http://api.weatherapi.com/v1/history.json?key=18163b2b531c4d2097941247212912' + '&q=' + country + '&q=' + city + '&dt=2021-12-28' + '&aqi=no')
+    .then(function(response) {
+      var jsonObject = JSON.stringify(response.data);
+      var object = JSON.parse(jsonObject);
+
+      // File path where data is to be written
+      // Here, we assume that the file to be in
+      // the same location as the .js file
+      var path = 'api-data.json';
+
+      // Declare a buffer and write the
+      // data in the buffer
+      let buffer = new Buffer.from(jsonObject);
+
+      // The fs.open() method takes a "flag"
+      // as the second argument. If the file
+      // does not exist, an empty file is
+      // created. 'a' stands for append mode
+      // which means that if the program is
+      // run multiple time data will be
+      // appended to the output file instead
+      // of overwriting the existing data.
+      fs.open(path, 'r+', function (err, fd) {
+
+        // If the output file does not exists
+        // an error is thrown else data in the
+        // buffer is written to the output file
+        if (err) {
+          console.log('Cant open file');
+        } else {
+          fs.write(fd, buffer, 0, buffer.length,
+            null, function (err, writtenbytes) {
+              if (err) {
+                console.log('Cant write to file');
+              } else {
+                console.log(writtenbytes +
+                  ' characters added to file');
+              }
+            })
+        }
+      });
+      
+      
+      var keysArray = Object.keys(object);
+
+      var location = new Object();
+      var forecast = new Object();
+
+      for(var i = 0; i < keysArray.length; i++) {
+        var key = keysArray[i];
+        if(key === 'location') {
+          location = object[key];
+        } else if(key === 'forecast') {
+          forecast = object[key].forecastday;
+          console.log(forecast.length);
+          console.log(forecast[0].date);
+          console.log(forecast[0].day.maxtemp_c);
+          console.log(object[key].forecastday.length);
+          console.log(object[key].forecastday[0].day);
+          console.log(object[key].forecastday[0].astro);
+          console.log(object[key].forecastday[0].hour.length);
+          console.log(object[key].forecastday[0].hour[0]);
+        }
+        
+      }
+
+      res.render('index', {title: "Saklain"});
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+})
 
 module.exports = router;
