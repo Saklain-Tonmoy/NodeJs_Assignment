eb28801bf23c3a4b2baa2c65f8b1e5a7f20ee32a
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2021-12-31 21:48:31 +0600

    commit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,11 @@
-$(document).onload(function () {
-    $('[name="graduate"]').change(function () {
-        if ($('[name="graduate"]:checked').is(":checked")) {
-            $('.celcius').hide();
-            $('.fahrenheit').show();
-        } else {
-            $('.celcius').show();
-            $('.fahrenheit').hide();
-        }
-    });
+$(document).ready(function () {
+$('[name="graduate"]').change(function () {
+if ($('[name="graduate"]:checked').is(":checked")) {
+$('.celcius').addClass('d-none');
+$('.fahrenheit').removeClass('d-none');
+} else {
+$('.celcius').removeClass('d-none');
+$('.fahrenheit').addClass('d-none');
+}
+});
 });
